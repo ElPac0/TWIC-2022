0cdf6d64c3ae139214ec17a4c05c59afd83a130e
Lucien <voltzlucien@gmail.com>
2022-04-30 23:07:46 +0200

    pagination

--- src/main/java/com/repository/Ville.java
+++ src/main/java/com/repository/Ville.java
@@ -2,14 +2,11 @@ package com.repository;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @Getter
 @Setter
